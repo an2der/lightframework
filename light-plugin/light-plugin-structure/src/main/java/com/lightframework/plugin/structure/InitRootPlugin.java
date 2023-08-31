780578168327a5c02cea841ca768c7999c04c672
yanggang <an2der@163.com>
2023-08-31 22:21:29 +0800

    1

---
+++ light-plugin/light-plugin-structure/src/main/java/com/lightframework/plugin/structure/InitRootPlugin.java
@@ -0,0 +1,22 @@
+package com.lightframework.plugin.structure;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/*** 
+ * @author yg
+ * @date 2023/8/31 21:28
+ * @version 1.0
+ */
+@Mojo(name = "init-root",defaultPhase = LifecyclePhase.NONE)
+public class InitRootPlugin extends AbstractMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        this.getLog().info("init root plugin!");
+    }
+
+}
