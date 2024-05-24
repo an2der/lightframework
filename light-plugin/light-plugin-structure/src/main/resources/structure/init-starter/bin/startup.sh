9dccafe63d4cefbbd7bb5342f456fb75cd68bb20
yanggang <an2der@163.com>
2024-05-24 23:50:23 +0800

    1

--- light-plugin/light-plugin-structure/src/main/resources/structure/init-starter/bin/startup.sh
+++ light-plugin/light-plugin-structure/src/main/resources/structure/init-starter/bin/startup.sh
@@ -21,7 +21,7 @@ if [ -n "$PIDS" ]; then
 fi
 
 echo -e "Starting the $SERVICE_NAME ...\c"
-nohup $JAVA_HOME/bin/java -jar -Xmx2g -Xms2g -Xmn1500m -XX:PermSize=100M -XX:MaxPermSize=200M -Xss256K -XX:+DisableExplicitGC -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=4 -XX:+CMSClassUnloadingEnabled -XX:LargePageSizeInBytes=128M -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=55 -XX:+PrintClassHistogram -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -Dlogging.config=file:./config/logback.xml $JAR_NAME >/dev/null 2>&1 &
+nohup $JAVA_HOME/bin/java -jar -Xmx2g -Xms2g -Xmn1500m -XX:PermSize=100M -XX:MaxPermSize=200M -Xss256K -XX:+DisableExplicitGC -XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=4 -XX:+CMSClassUnloadingEnabled -XX:LargePageSizeInBytes=128M -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=55 -XX:+PrintClassHistogram -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintHeapAtGC -Dlogging.config=file:./config/log4j2.xml $JAR_NAME >/dev/null 2>&1 &
 
 echo "OK!"
 PIDS=`ps -ef | grep $SERVICE_NAME | grep -v "grep" | awk '{print $2}'`
