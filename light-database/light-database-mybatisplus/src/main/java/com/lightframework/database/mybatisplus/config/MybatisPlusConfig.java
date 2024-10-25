0169c1f894b2e2d055e0c940c1b2c1d1705b30d0
yanggang <an2der@163.com>
2024-10-25 17:15:44 +0800

    1.0.6

--- light-database/light-database-mybatisplus/src/main/java/com/lightframework/database/mybatisplus/config/MybatisPlusConfig.java
+++ light-database/light-database-mybatisplus/src/main/java/com/lightframework/database/mybatisplus/config/MybatisPlusConfig.java
@@ -3,6 +3,7 @@ package com.lightframework.database.mybatisplus.config;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +16,7 @@ public class MybatisPlusConfig {
      * @return
      */
     @Bean
+    @ConditionalOnMissingBean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
