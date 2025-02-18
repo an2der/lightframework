7d8a0960f7b0107db54fb99064005c5eeff8a5f1
yanggang <an2der@163.com>
2025-02-18 10:30:03 +0800

    1.0.21

--- light-module/light-web/light-web-core/src/main/java/com/lightframework/web/core/handler/GlobalExceptionHandler.java
+++ light-module/light-web/light-web-core/src/main/java/com/lightframework/web/core/handler/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.lightframework.web.core.handler;
 
-import cn.hutool.json.JSONUtil;
 import com.lightframework.web.common.BusinessException;
 import com.lightframework.web.common.BusinessResponse;
 import com.lightframework.web.common.BusinessStatus;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-import java.util.Arrays;
 import java.util.stream.Collectors;
 
 /** 全局异常处理
@@ -31,28 +29,53 @@ public class GlobalExceptionHandler {
 
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
+    /**
+     * 业务异常处理
+     * @param e
+     * @return
+     */
     @ExceptionHandler(BusinessException.class)
     public BusinessResponse businessExceptionHandler(BusinessException e){
         return new BusinessResponse(e);
     }
 
+    /**
+     * 参数校验异常处理
+     * @param e
+     * @return
+     */
     @ExceptionHandler({MethodArgumentNotValidException.class, BindException.class})
     public BusinessResponse bindExceptionHandler(BindException e){
         return new BusinessResponse(BusinessStatus.BAD_REQUEST.getCode(), e.getBindingResult().getFieldErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.joining(";")));
     }
 
+    /**
+     * 参数校验异常处理
+     * @param e
+     * @return
+     */
     @ExceptionHandler({HttpMessageNotReadableException.class, MissingServletRequestParameterException.class})
     public BusinessResponse httpMessageNotReadableExceptionHandler(Exception e){
         return new BusinessResponse(BusinessStatus.BAD_REQUEST);
     }
 
+    /**
+     * 参数校验异常处理
+     * @param e
+     * @return
+     */
     @ExceptionHandler(ConstraintViolationException.class)
     public BusinessResponse constraintViolationExceptionHandler(ConstraintViolationException e){
         return new BusinessResponse(BusinessStatus.BAD_REQUEST.getCode(),e.getConstraintViolations().stream()
                 .map(ConstraintViolation::getMessage).collect(Collectors.joining(";")));
     }
 
+    /**
+     * 默认异常处理
+     * @param e
+     * @return
+     */
     @ExceptionHandler(Exception.class)
     public BusinessResponse exceptionHandler(Exception e){
         log.error(e.getMessage(),e);
