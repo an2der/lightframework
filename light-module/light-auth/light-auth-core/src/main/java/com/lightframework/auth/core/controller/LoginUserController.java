c843cd56eb2a62d0ef445324ba9229874b6d331b
yanggang <an2der@163.com>
2025-05-12 16:51:51 +0800

    1.0.25

--- light-module/light-auth/light-auth-core/src/main/java/com/lightframework/auth/core/controller/LoginUserController.java
+++ light-module/light-auth/light-auth-core/src/main/java/com/lightframework/auth/core/controller/LoginUserController.java
@@ -1,6 +1,8 @@
 package com.lightframework.auth.core.controller;
 
 import com.lightframework.auth.common.model.UserInfo;
+import com.lightframework.web.common.BusinessType;
+import com.lightframework.web.common.annotation.SystemLogger;
 import com.lightframework.web.core.annotation.BusinessController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginUserController {
 
     @GetMapping("/userinfo")
+    @SystemLogger(moduleKey = "USER", moduleName = "用户",operationDesc = "获取登录用户信息",businessType = BusinessType.SELECT)
     public UserInfo userinfo(UserInfo userInfo){
         return userInfo;
     }
